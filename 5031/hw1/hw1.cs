9c7359a064439f49fc92d3a1da177d66bc82ae76
Cecilia Garcia Lopez de Munain <ceci.garcialm@gmail.com>
2023-04-03 23:48:29 -0700

    Minor changes

--- 5031/hw1/hw1.cs
+++ 5031/hw1/hw1.cs
@@ -4,7 +4,7 @@
     /// Calculates the n-th Fibonacci number recursively using the classic approach.
     /// </summary>
     /// <param name="n">The index of the Fibonacci number to calculate.</param>
-    /// <param name="steps">A reference to an integer that will store the number of operations performed.</param>
+    /// <param name="steps">A reference to an integer that counts the number of addition operations.</param>
     /// <returns>The n-th Fibonacci number.</returns>
     public static int FibRecursive(int n, ref int steps)
     {
@@ -24,7 +24,7 @@
     /// Calculates the n-th Fibonacci number iteratively using a for loop.
     /// </summary>
     /// <param name="n">The index of the Fibonacci number to calculate.</param>
-    /// <param name="steps">A reference to an integer that will store the number of operations performed.</param>
+    /// <param name="steps">A reference to an integer that counts the number of addition operations.</param>
     /// <returns>The n-th Fibonacci number.</returns>
     public static int FibIterative(int n, ref int steps)
     {
@@ -36,10 +36,10 @@
         int a = 0, b = 1, res = 1;
         for (int i = 2; i <= n; i++)
         {
+            steps++;
             res = a + b;
             a = b;
             b = res;
-            steps++;
         }
 
         return res;
@@ -49,7 +49,7 @@
     /// Calculates the n-th Fibonacci number recursively using an accumulation approach.
     /// </summary>
     /// <param name="n">The index of the Fibonacci number to calculate.</param>
-    /// <param name="steps">A reference to an integer that will store the number of operations performed.</param>
+    /// <param name="steps">A reference to an integer that counts the number of addition operations.</param>
     /// <param name="a">The accumulator for the previous Fibonacci number.</param>
     /// <param name="b">The accumulator for the current Fibonacci number.</param>
     /// <returns>The n-th Fibonacci number.</returns>
@@ -70,7 +70,7 @@ class Program
 {
     /// <summary>
     /// The main entry point of the program. Prompts the user for positive integers and calculates the corresponding
-    /// Fibonacci numbers using different methods, displaying the number of operations performed for each method.
+    /// Fibonacci numbers using different methods, displaying the number of addition operations performed for each method.
     /// </summary>
     /// <param name="args">The command line arguments (not used).</param>
     static void Main(string[] args)
@@ -93,8 +93,7 @@ class Program
 
         Console.WriteLine("Number of addition operations in… ");
         var linePattern = "|{0,10}|{1,10}|{2,20}|{3,20}|{4,20}|";
-        Console.WriteLine(String.Format(linePattern, "n", "fib(n)", "Classic Recursive", "Iterative",
-            "Recursive w/ Accum"));
+        Console.WriteLine(String.Format(linePattern, "n", "fib(n)", "Classic Recursive", "Iterative", "Recursive w/ Accum"));
         foreach (int num in numbers)
         {
             int stepsR = 0, stepsI = 0, stepsRA = 0;
@@ -107,7 +106,6 @@ class Program
                 Console.WriteLine("There was an error in the calculation of Fibonacci for " + num + ".");
                 break;
             }
-
             Console.WriteLine(String.Format(linePattern, num, fibR, stepsR, stepsI, stepsRA));
         }
     }
