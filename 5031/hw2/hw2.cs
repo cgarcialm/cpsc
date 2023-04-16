8abe67a6b8aeb61927d80d8b307949125a9da7ca
Cecilia Garcia Lopez de Munain <ceci.garcialm@gmail.com>
2023-04-15 21:55:25 -0700

    Initial commit of hw2

---
+++ 5031/hw2/hw2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace hw2;
+class Program
+{
+    public static int BruteForceStringMatch(string totalString, string lookUpString) {
+        for (int i = 0; i < totalString.Length - lookUpString.Length; i++) {
+            int j = 0;
+            while(j<lookUpString.Length && lookUpString[j] == totalString[j]) {
+                j++;
+            }
+            if (j == totalString.Length) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    static void Main(string[] args)
+    {
+        string totalString = "hola";
+        string lookUpString = "la";
+
+        Console.WriteLine(BruteForceStringMatch(totalString, lookUpString));
+    }
+}
