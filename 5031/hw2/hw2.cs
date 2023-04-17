3da12b11f3ba1c20e17948df16dbef26308b4fb9
Cecilia Garcia Lopez de Munain <ceci.garcialm@gmail.com>
2023-04-16 20:54:35 -0700

    Improved version with user input

--- 5031/hw2/hw2.cs
+++ 5031/hw2/hw2.cs
@@ -3,13 +3,30 @@
 namespace hw2;
 class Program
 {
+    static string PromptUserInput(string inputName) {
+        string userInput;
+        Console.Write(string.Format("Enter a {0}: ", inputName));
+        userInput = Console.ReadLine();
+
+        return userInput;
+    }
+
+    static void PrintStringMatchResults(List<string> totalStrings, List<string> lookUpStrings, List<int> resultIndexes) {
+        Console.WriteLine("Number of addition operations in… ");
+        var linePattern = "|{0,-20}|{1,-20}|{2,20}|";
+        Console.WriteLine(String.Format(linePattern, "S", "U", "Result"));
+        for(int i = 0; i < totalStrings.Count; i++) {
+            Console.WriteLine(String.Format(linePattern, totalStrings[i], lookUpStrings[i], resultIndexes[i]));
+        }
+    }
+
     public static int BruteForceStringMatch(string totalString, string lookUpString) {
-        for (int i = 0; i < totalString.Length - lookUpString.Length; i++) {
+        for (int i = 0; i <= totalString.Length - lookUpString.Length; i++) {
             int j = 0;
-            while(j<lookUpString.Length && lookUpString[j] == totalString[j]) {
+            while(j<lookUpString.Length && lookUpString[j] == totalString[i+j]) {
                 j++;
             }
-            if (j == totalString.Length) {
+            if (j == lookUpString.Length) {
                 return i;
             }
         }
@@ -18,9 +35,28 @@ class Program
 
     static void Main(string[] args)
     {
-        string totalString = "hola";
-        string lookUpString = "la";
+        Console.WriteLine("Welcome to String Match.");
+        Console.WriteLine("You'll be asked to input string and the substring to look up:\n");
+        List<string> totalStrings = new List<string>();
+        List<string> lookUpStrings = new List<string>();
+        List<int> resultIndexes = new List<int>();
+        int stringMatchNumber = 0; 
 
-        Console.WriteLine(BruteForceStringMatch(totalString, lookUpString));
+        string? run = "y";
+        do {
+            totalStrings.Add(PromptUserInput("string"));
+            lookUpStrings.Add(PromptUserInput("lookup substring"));
+            resultIndexes.Add(BruteForceStringMatch(totalStrings[stringMatchNumber], lookUpStrings[stringMatchNumber]));
+            stringMatchNumber++;
+
+            Console.Write("\nDo you want to match more strings? y/n: ");
+            run = Console.ReadLine();
+            while (run != "y" && run != "n") {
+                    Console.Write("Invalid input. Please enter \"y\" or \"n\": ");
+                    run = Console.ReadLine();
+                }
+        } while (run == "y");
+        PrintStringMatchResults(totalStrings, lookUpStrings, resultIndexes);
+        Console.WriteLine("Goodbye!");
     }
 }
