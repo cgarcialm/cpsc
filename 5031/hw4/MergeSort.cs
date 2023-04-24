6d83c1bdbabb8f846cf0bc6acb13539a8540de39
Cecilia Garcia Lopez de Munain <ceci.garcialm@gmail.com>
2023-04-24 12:07:27 -0700

    Add base code for mergesort

---
+++ 5031/hw4/MergeSort.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class MergeSort {
+    int[] A;
+
+    MergeSort(int[] anArray) {
+        A = anArray;
+    }
+
+    public int[] sort() {
+        return A;
+    }
+
+    static void Main(string[] args) {
+        
+    }
+}
