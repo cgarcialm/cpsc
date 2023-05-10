39b488f7e55dcf7fccf7dd693c790ba1f48baab0
Cecilia Garcia Lopez de Munain <ceci.garcialm@gmail.com>
2023-05-10 11:18:00 -0700

    Add first Heapsort class

---
+++ 5031/hw5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class HeapSort{
+
+    int[] array;
+
+    HeapSort(int[] anArray) {
+        array = new int[anArray.Length];
+        for(int i = 0; i < anArray.Length; i++) {
+            array[i] = anArray[i];
+        }
+    }
+}
