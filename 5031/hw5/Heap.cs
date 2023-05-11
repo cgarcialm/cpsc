9fcfc5738eee3175c2fdd8c3023f4b5a832b4ad3
Cecilia Garcia Lopez de Munain <ceci.garcialm@gmail.com>
2023-05-10 19:37:13 -0700

    Add documentation to heap

--- 5031/hw5/Heap.cs
+++ 5031/hw5/Heap.cs
@@ -1,9 +1,35 @@
-﻿using System;
+﻿/******************************************************************************
 
+ * Author: Cecilia Garcia Lopez de Munain <cgarcialopezdemunain@seattleu.edu>
+ * Date: May 10 2023
+ * Platform: MacOS Monterrey. Version 12.0.1.
+ * Version: 1.0
+ * Purpose: Implementation of HeapSort.
+
+Implementation of HeapSort per the algorithm described in the book "Introduction
+to The Design and Analysis of Algorithms" by Anany Levitin, 3rd edition, to sort 
+an array in ascending order.
+
+*******************************************************************************/
+
+using System;
+
+/// <summary>
+/// Class Heap implements a binary tree that always mantains the following 
+/// properties:
+///     1. It is complete
+///     2. It satisfies heap-order property: Data in each node >= data in 
+///     children
+/// </summary>
 class Heap{
 
     private int[] H;
 
+    /// <summary>
+    /// Constructs a heap from elements of a given array using bottom-up heap 
+    /// construction
+    /// </summary>
+    /// <param name="anArray">Array of ints to create heap</param>
     public Heap(int[] anArray) {
         H = new int[anArray.Length];
         for(int i = 0; i < anArray.Length; i++) {
@@ -13,13 +39,17 @@ class Heap{
     }
 
     /// <summary>
-    /// Checks if length of H is 0
+    /// Checks if length of heap is 0
     /// </summary>
-    /// <returns>Boolean indicating if H is empty</returns>
+    /// <returns>Boolean indicating if heap is empty</returns>
     public bool empty() {
         return H.Length == 0;
     }
 
+    /// <summary>
+    /// Generates string represatation of heap
+    /// </summary>
+    /// <returns></returns>
     public string toString()
     {
         string s = "{";
@@ -32,6 +62,18 @@ class Heap{
         return s;
     }
 
+    /// <summary>
+    /// Retrieves the max item
+    /// </summary>
+    /// <returns>Max item</returns>
+    public int getMax() {
+        return H[0];
+    }
+
+    /// <summary>
+    /// Deletes the max item
+    /// </summary>
+    /// <returns>Max item</returns>
     public int delete() {
         int max = H[0];
         H[0] = H[H.Length - 1];
@@ -40,12 +82,34 @@ class Heap{
         return max;
     }
 
+    /// <summary>
+    /// Insert value to heap
+    /// </summary>
+    /// <param name="v">Value to insert</param>
     public void insert(int v) {
         Array.Resize(ref H, H.Length + 1);
         H[H.Length - 1] = v;
         percolateUp(H.Length - 1);
     }
 
+    /// <summary>
+    /// Generates sorted array from heap
+    /// </summary>
+    /// <returns>Sorted array of ints</returns>
+    public int[] sort() {
+        int initLength = H.Length;
+        int[] sortedArray = new int[initLength];
+        for(int i = 0; i < initLength; i++) {
+            sortedArray[i] = delete();
+        }
+        return sortedArray;
+    }
+
+    /// <summary>
+    /// Swaps node k with the biggest of its children until array is in heap 
+    /// order
+    /// </summary>
+    /// <param name="k">Index of element to percolate</param>
     private void percolateDown(int k) {
         int v = H[k];
         bool heap = false;
@@ -66,6 +130,10 @@ class Heap{
         H[k] = v;
     }
 
+    /// <summary>
+    /// Swaps node k with its parent until array is in heap order
+    /// </summary>
+    /// <param name="k">Index of element to percolate</param>
     private void percolateUp(int k) {
         int v = H[k];
         bool heap = false;
@@ -81,16 +149,28 @@ class Heap{
         }
     }
 
+    /// <summary>
+    /// Rearranges array H to put it in heap order
+    /// </summary>
     private void heapify() {
-        int i = (H.Length - 1)/2;
-        while(i >= 0) {
-            percolateDown(i);
-            i--;
+        if(H.Length > 1) {
+            int i = (H.Length - 1)/2;
+            while(i >= 0) {
+                percolateDown(i);
+                i--;
+            }
         }
     }
 }
 
+/// <summary>
+/// Class Homework 5 tests the implementation of Heap.
+/// </summary>
 class Homework5 {
+    /// <summary>
+    /// The main entry point of the program.
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args) {
         Heap h = new Heap(new int[] {2, 9, 7, 6, 5, 8});
         Console.WriteLine("Heap: " + h.toString());
@@ -99,5 +179,6 @@ class Homework5 {
         Console.WriteLine("Inserted: 10");
         h.insert(10);
         Console.WriteLine("Heap: " + h.toString());
+        int[] sorted = h.sort();
     }
 }
