ac72fb7198f581b60b3a83a77cc0723971a85e3d
Cecilia Garcia Lopez de Munain <ceci.garcialm@gmail.com>
2023-05-16 14:22:09 -0700

    Fix nodes calculation and fix string dot processing

--- 5031/hw6/Program.cs
+++ 5031/hw6/Program.cs
@@ -1,46 +1,50 @@
 ﻿using System;
+using System.Diagnostics;
 
 class MatrixReader {
 
     string fileName;
+    int nodes;
     int[,] array;
     string dotString;
 
     public MatrixReader(string fileName) {
-        this.fileName = "input/" + fileName;
+        this.fileName = fileName; // TODO: check if txt
         to2DArray();
         createDotString();
     }
 
     private void to2DArray() {
-        string input = File.ReadAllText(fileName);
+        string input = File.ReadAllText("input/" + fileName);
         string[] stringRows = input.Split('\n');
-        int nodes = stringRows.Length;
+        nodes = stringRows.Length - 1;
 
         array = new int[nodes, nodes];
         for(int i = 0; i < nodes; i++) {
             string[] stringCols = stringRows[i].Split('\t');
             for(int j = 0; j < nodes; j++) {
-                array[i, j] = Int32.Parse(stringCols[j]);
+                array[i, j] = Int32.Parse(stringCols[j]); // TODO: check if int
             }
         }
     }
 
     private void createDotString() {
-        dotString = "diagraph D { \n";
+        dotString = "digraph { \n";
         char[] az = Enumerable.Range('a', array.Length).Select(i => (Char)i).ToArray();
-        for (int i = 0; i < array.Length; i++) {
-            for (int j = 0; j < array.Length; j++) {
-                dotString += az[i] + "->" + az[j] + "\n";
+        for (int i = 0; i < nodes; i++) {
+            for (int j = 0; j < nodes; j++) {
+                if (array[i, j] == 1) {
+                    dotString += az[i] + "->" + az[j] + "\n";
+                }
             }
         }
-        dotString += "}";
+        dotString += " }";
     }
 }
 
 class Homework6 {
 
     static void Main(string[] args) {
-        MatrixReader mr = new MatrixReader("adj1.txt");
+        MatrixReader mr = new MatrixReader("adj3.txt");
     }
 }
