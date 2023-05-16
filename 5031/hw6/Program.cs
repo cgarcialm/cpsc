8cc4387f7ef3eb73c6f823fbe582fc3c32f8707a
Cecilia Garcia Lopez de Munain <ceci.garcialm@gmail.com>
2023-05-16 12:34:27 -0700

    Add dotString generator method to matrix reader

--- 5031/hw6/Program.cs
+++ 5031/hw6/Program.cs
@@ -4,10 +4,12 @@ class MatrixReader {
 
     string fileName;
     int[,] array;
+    string dotString;
 
     public MatrixReader(string fileName) {
         this.fileName = "input/" + fileName;
         to2DArray();
+        createDotString();
     }
 
     private void to2DArray() {
@@ -22,6 +24,17 @@ class MatrixReader {
                 array[i, j] = Int32.Parse(stringCols[j]);
             }
         }
+    }
+
+    private void createDotString() {
+        dotString = "diagraph D { \n";
+        char[] az = Enumerable.Range('a', array.Length).Select(i => (Char)i).ToArray();
+        for (int i = 0; i < array.Length; i++) {
+            for (int j = 0; j < array.Length; j++) {
+                dotString += az[i] + "->" + az[j] + "\n";
+            }
+        }
+        dotString += "}";
     }
 }
 
