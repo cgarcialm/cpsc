71953ce769b446c949d94454da6c1c2c0a0b8908
Cecilia Garcia Lopez de Munain <ceci.garcialm@gmail.com>
2023-05-16 12:20:47 -0700

    Add first version of Homework6

---
+++ 5031/hw6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class MatrixReader {
+
+    string fileName;
+    int[,] array;
+
+    public MatrixReader(string fileName) {
+        this.fileName = "input/" + fileName;
+        to2DArray();
+    }
+
+    private void to2DArray() {
+        string input = File.ReadAllText(fileName);
+        string[] stringRows = input.Split('\n');
+        int rows = stringRows.Length;
+        int cols = stringRows[0].Split('\t').Length;
+
+        array = new int[rows, cols];
+        for(int i = 0; i < rows; i++) {
+            string[] stringCols = stringRows[i].Split('\t');
+            for(int j = 0; j < cols; j++) {
+                array[i, j] = Int32.Parse(stringCols[j]);
+            }
+        }
+    }
+}
+
+class Homework6 {
+
+    static void Main(string[] args) {
+        MatrixReader mr = new MatrixReader("adj1.txt");
+    }
+}
