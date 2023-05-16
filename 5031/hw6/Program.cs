f37f9904661375c23bd0755ffaa79d3bb2be4214
Cecilia Garcia Lopez de Munain <ceci.garcialm@gmail.com>
2023-05-16 12:26:35 -0700

    Change matrix reader to have square matrix n elements

--- 5031/hw6/Program.cs
+++ 5031/hw6/Program.cs
@@ -13,13 +13,12 @@ class MatrixReader {
     private void to2DArray() {
         string input = File.ReadAllText(fileName);
         string[] stringRows = input.Split('\n');
-        int rows = stringRows.Length;
-        int cols = stringRows[0].Split('\t').Length;
+        int nodes = stringRows.Length;
 
-        array = new int[rows, cols];
-        for(int i = 0; i < rows; i++) {
+        array = new int[nodes, nodes];
+        for(int i = 0; i < nodes; i++) {
             string[] stringCols = stringRows[i].Split('\t');
-            for(int j = 0; j < cols; j++) {
+            for(int j = 0; j < nodes; j++) {
                 array[i, j] = Int32.Parse(stringCols[j]);
             }
         }
