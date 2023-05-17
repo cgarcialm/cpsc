598133d9a1d9ee2735dc5deeb7a66d5e970498a4
Cecilia Garcia Lopez de Munain <ceci.garcialm@gmail.com>
2023-05-16 19:10:03 -0700

    Add documentation to graph viz generator

--- 5031/hw6/GraphVizGenerator.cs
+++ 5031/hw6/GraphVizGenerator.cs
@@ -1,6 +1,24 @@
-﻿using System;
+﻿/******************************************************************************
+
+ * Author: Cecilia Garcia Lopez de Munain <cgarcialopezdemunain@seattleu.edu>
+ * Date: May 16 2023
+ * Platform: MacOS Monterrey. Version 12.0.1.
+ * Version: 1.0
+ * Purpose: Implementation of GraphVizGenerator program.
+
+Welcome to GraphVizGenerator. GraphVizGenerator is a program that takes a text 
+input with a matrix and uses GraphViz toolkit to create a diagram of the graph 
+that it represents.
+
+*******************************************************************************/
+
+using System;
 using System.Diagnostics;
 
+/// <summary>
+/// MatrixException implements an Exception that prints an error if the input 
+/// matrix does not comply with the requirements of GraphVizGenerator
+/// </summary>
 class MatrixException : Exception {
     public MatrixException()
     {
@@ -11,21 +29,30 @@ class MatrixException : Exception {
     }
 }
 
+/// <summary>
+/// GraphVizGenerator implements a program to take a text input with a square matrix 
+/// of 0 and 1 values and generate the corresponding representation of the graph in 
+/// a diagram.
+/// </summary>
 class GraphVizGenerator {
 
     string fileName;
     int nodes;
     bool symmetric = true;
     int[,] array;
-    string dotString;
+    string dotGraphString;
 
     public GraphVizGenerator(string fileName) {
         this.fileName = fileName;
         to2DArray();
         isMatrixSymmetric();
-        createDotString();
+        createdotGraphString();
     }
 
+    /// <summary>
+    /// Transforms text input of square matrix into a 2D array and stores it
+    /// in member variable array.
+    /// </summary>
     private void to2DArray() {
         string input = File.ReadAllText("input/" + fileName).Trim('\r', '\n');
         string[] stringRows = input.Split('\n'); 
@@ -40,7 +67,7 @@ class GraphVizGenerator {
             }
             for(int j = 0; j < nodes; j++) {
                 try {
-                    array[i, j] = Int32.Parse(stringCols[j]);
+                    array[i, j] = Int32.Parse(stringCols[j]); // TODO: Throw exception if input is not 0 or 1
                 } catch (Exception e) {
                     throw new MatrixException("Value \"" + stringCols[j] + "\" is not an integer.");
                 }
@@ -48,6 +75,10 @@ class GraphVizGenerator {
         }
     }
 
+    /// <summary>
+    /// Checks if matrix stored in array is symmetric and updates member variable 
+    /// symmetric.
+    /// </summary>
     private void isMatrixSymmetric() {
         for(int i = 0; i < nodes; i++) {
             for(int j = i + 1; j < nodes; j++) {
@@ -59,8 +90,13 @@ class GraphVizGenerator {
         }
     }
 
-    private void createDotString() {
-        dotString = symmetric ? "graph { \n" : "digraph { \n";
+    /// <summary>
+    /// Creates a string representing the graph diagram with the format 
+    /// required by GraphViz toolkit.
+    /// Stores the corresponding string in member variable dotGraphString.
+    /// </summary>
+    private void createdotGraphString() {
+        dotGraphString = symmetric ? "graph { \n" : "digraph { \n";
         string edge = symmetric ? "--" : "->";
         char[] az = Enumerable.Range('a', array.Length).Select(i => (Char)i).ToArray();
 
@@ -68,28 +104,36 @@ class GraphVizGenerator {
             if (symmetric) {
                 for (int j = i; j < nodes; j++) {
                     if (array[i, j] == 1) {
-                        dotString += az[i] + edge + az[j] + "\n";
+                        dotGraphString += az[i] + edge + az[j] + "\n";
                     }
                 }
             } else {
                 for (int j = 0; j < nodes; j++) {
                     if (array[i, j] == 1) {
-                        dotString += az[i] + edge + az[j] + "\n";
+                        dotGraphString += az[i] + edge + az[j] + "\n";
                     }
                 }
             }
         }
-        dotString += " }";
+        dotGraphString += " }";
     }
 
+    /// <summary>
+    /// Cretes the shell command to be ran in terminal to create the graph diagram.
+    /// </summary>
+    /// <returns></returns>
     private string createShellCommand() {
-        string strCmdText = "echo '" + dotString + "' | dot -Tpng > output/" + fileName.Substring(0, fileName.LastIndexOf(".")) + ".png";
-        File.WriteAllText("output/" + fileName , strCmdText);
+        string strCmdText = "echo '" + dotGraphString + "' | dot -Tpng > output/" + fileName.Substring(0, fileName.LastIndexOf(".")) + ".png";
+        File.WriteAllText("output/" + fileName , strCmdText); // TODO: Drop this export
 
         return strCmdText;
     }
 
-    public string generate()
+    /// <summary>
+    /// Runs GraphVizGenerator: creates shell command and runs it in terminal,
+    /// storing png outputs to output folder.
+    /// </summary>
+    public void run()
     {
         string command = createShellCommand().Replace("\"","\"\"");
 
@@ -106,21 +150,24 @@ class GraphVizGenerator {
 
         proc.Start();
         proc.WaitForExit();
-
-        return proc.StandardOutput.ReadToEnd();
     }
-    
 }
 
+/// <summary>
+/// Homework6 runs GraphVizGenerator on a set of inputs stored in List testInputs.
+/// </summary>
 class Homework6 {
-
+    /// <summary>
+    /// Main entry of the program. Creates a List of input file names and generates 
+    /// its corresponding graph diagrams.
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args) {
-
         List<string> testInputs = new List<string> {"adj1.txt", "adj2.txt", "adj3.txt", "adj4.txt"};
 
         foreach(string input in testInputs) {
             GraphVizGenerator mr = new GraphVizGenerator(input);
-            mr.generate();
+            mr.run();
         }
     }
 }
