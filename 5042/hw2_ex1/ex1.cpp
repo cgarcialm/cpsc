10b13d15d28e2d9a7f2ea957b2ebe435aa4738aa
Cecilia Garcia Lopez de Munain <ceci.garcialm@gmail.com>
2023-05-22 20:48:56 -0700

    Add documentation to threads ex1

--- 5042/hw2_ex1/ex1.cpp
+++ 5042/hw2_ex1/ex1.cpp
@@ -1,35 +1,45 @@
-// C++ program to demonstrate
-// multithreading using three
-// different callables.
+/*
+ * Author: Cecilia Garcia Lopez de Munain <cgarcialopezdemunain@seattleu.edu>
+ * Date: May 22, 2023
+ * Platform: macOS Monterrey
+ * Version: 1.0
+ *
+ * The code takes a list of numbers as command-line arguments and calculates the average, minimum, and maximum values
+ * using separate threads in C++. The numbers are stored in a struct, and each calculation is performed by a dedicated
+ * function. The results are then displayed on the console.
+ */
 #include <iostream>
 #include <thread>
 using namespace std;
 
-int average;
-int minimum;
-int maximum;
+int average; // Global variable to store the average
+int minimum; // Global variable to store the minimum
+int maximum; // Global variable to store the maximum
 
+/// Struct to hold an array and its length for merging
 struct Numbers {
-    int count;
+    int length;
     int* array;
 };
 
-// Function to calculate the average
+/// Calculates the average of the values stored in the array of a Numbers object.
+/// \param numbers
 void calculateAverage(Numbers numbers)
 {
     int total = 0;
-    for (int i = 0; i < numbers.count; i++)
+    for (int i = 0; i < numbers.length; i++)
     {
         total += numbers.array[i];
     }
-    average = total / numbers.count;
+    average = total / numbers.length;
 }
 
-// Function to calculate the minimum
+/// Calculates the minimum of the values stored in the array of a Numbers object.
+/// \param numbers
 void calculateMin(Numbers numbers)
 {
     minimum = numbers.array[0];
-    for (int i = 1; i < numbers.count; i++)
+    for (int i = 1; i < numbers.length; i++)
     {
         if(numbers.array[i] < minimum) {
             minimum = numbers.array[i];
@@ -37,11 +47,12 @@ void calculateMin(Numbers numbers)
     }
 }
 
-// Function to calculate the minimum
+/// Calculates the maximum of the values stored in the array of a Numbers object.
+/// \param numbers
 void calculateMax(Numbers numbers)
 {
     maximum = numbers.array[0];
-    for (int i = 1; i < numbers.count; i++)
+    for (int i = 1; i < numbers.length; i++)
     {
         if(numbers.array[i] > maximum) {
             maximum = numbers.array[i];
@@ -49,7 +60,12 @@ void calculateMax(Numbers numbers)
     }
 }
 
-// Driver code
+/// Driver program. Takes values from command line input and stores them in a Numbers object.
+/// In three different threads calculates the average, the minimum and the maximum.
+/// Waits for every thread to finish and prints the results.
+/// \param argc
+/// \param argv
+/// \return
 int main(int argc, char* argv[])
 {
     // Check if enough arguments are provided
@@ -58,15 +74,15 @@ int main(int argc, char* argv[])
         return 0;
     }
 
-    int count = argc - 1;
-    int* array = new int[count];
+    int length = argc - 1;
+    int* array = new int[length];
 
     // Parse the command-line arguments as numbers
     for (int i = 1; i < argc; ++i) {
         array[i - 1] = std::stoi(argv[i]);
     }
 
-    Numbers numbers = Numbers{ count, array };
+    Numbers numbers = Numbers{ length, array };
 
     // Create a threads and pass the arguments
     thread threadAverage(calculateAverage, numbers);
@@ -82,6 +98,8 @@ int main(int argc, char* argv[])
     std::cout << "The minimum value is " << minimum << std::endl;
     std::cout << "The maximum value is " << maximum << std::endl;
 
+    /// Clean up memory
     delete[] array;
+
     return 0;
 }
