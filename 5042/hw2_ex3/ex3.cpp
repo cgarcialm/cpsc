00eaeac52b11e84367effa729fa720993b2e776a
Cecilia Garcia Lopez de Munain <ceci.garcialm@gmail.com>
2023-05-23 00:29:51 -0700

    Adjusted thread sleeps and correct printing ex3

--- 5042/hw2_ex3/ex3.cpp
+++ 5042/hw2_ex3/ex3.cpp
@@ -27,20 +27,22 @@ void runner(int ID)
     string turnString = "Thread " + to_string(ID) + "'s turn!\n";
     string completedString = "Thread " + to_string(ID) + " completed.\n";;
     int count = 0;
-    while(count < 2) {
+    while(count < 3) {
         // Check if it's the thread's turn
         if (allowedID != ID) {
             cout << waitString;
-            this_thread::sleep_for(chrono::milliseconds (300)); // Delay to allow other threads to progress
         } else {
             cout << turnString;
             mtx.lock(); // Acquire the lock to update the shared variable
             count++;
             allowedID < 3 ? allowedID++ : allowedID = 1; // Update the shared variable, wrapping around from 3 to 1
             mtx.unlock(); // Release the lock
+            if(count == 3) {
+                cout << completedString;
+            }
         }
+        this_thread::sleep_for(chrono::milliseconds (2)); // Delay to allow other threads to progress
     }
-    cout << completedString;
 }
 
 /// Driver program. Creates three threads that run runner function and waits until they are completed.
@@ -51,7 +53,9 @@ int main(int argc, char* argv[])
 {
     // Create three threads and pass the ID as argument
     thread thread1(runner, 1);
+    this_thread::sleep_for(chrono::milliseconds (1)); // Delay to allow previous thread to do something
     thread thread2(runner, 2);
+    this_thread::sleep_for(chrono::milliseconds (1)); // Delay to allow previous thread to do something
     thread thread3(runner, 3);
 
     // Wait for the threads to finish
