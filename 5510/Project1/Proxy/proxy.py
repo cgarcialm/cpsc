af0df5ff25aa9f87439fd0d0fc14b14cdf4554e0
Cecilia Garcia Lopez de Munain <ceci.garcialm@gmail.com>
2023-10-07 20:42:49 -0700

    Add message validation to proxy

--- 5510/Project1/Proxy/proxy.py
+++ 5510/Project1/Proxy/proxy.py
@@ -1,6 +1,27 @@
 from socket import *
 import sys
 
+
+def is_msg_length_valid(msg):
+    return len(msg.split()) == 3
+
+def parse_http_msg(msg):
+
+  msg_components = msg.split()
+  
+  method = msg_components[0]
+  url = msg_components[1]
+  version = msg_components[2]
+
+  return method, url, version
+
+def is_http_version_correct(version):
+  return version == 'HTTP/1.1'
+
+def is_method_get(method):
+  return method == 'GET'
+
+
 if __name__ == "__main__":
   print('Number of arguments:', len(sys.argv), 'arguments.')
   print('Argument List:', str(sys.argv))
@@ -25,8 +46,18 @@ if __name__ == "__main__":
     client_msg = conn_socket.recv(1024).decode()
     print('client msg: {}'.format(client_msg))
 
+    if not is_msg_length_valid(client_msg):
+      server_msg = 'Message length incorrect. Should be 3.'.encode()
+    else:
+      method, url, version = parse_http_msg(client_msg)
+      if not is_method_get(method):
+        server_msg = 'Method incorrect. Should be GET.'.encode()
+      elif not is_http_version_correct(version):
+        server_msg = 'HTTP version incorrect. Should be HTTP/1.1.'.encode()
+      else:
+        server_msg = 'Message read in 127.0.0.1 {}'.format(sys.argv[1]).encode()
+    
     # Send back msg
-    server_msg = 'Message read in 127.0.0.1 {}'.format(sys.argv[1]).encode()
     conn_socket.send(server_msg)
 
     # Close connection socket
