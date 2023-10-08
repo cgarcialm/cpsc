5fe4f53273fd62c3bea0b6fa4a73c9f937cebdb2
Cecilia Garcia Lopez de Munain <ceci.garcialm@gmail.com>
2023-10-08 02:38:02 -0700

    Update cliente code

--- 5510/Project1/Client/client.py
+++ 5510/Project1/Client/client.py
@@ -1,15 +1,15 @@
 from socket import *
 
 if __name__ == "__main__":
-  # server_name = 'zhiju.me'
-  # server_ip = '54.218.17.199'
   proxy_ip = ''
   proxy_port = 65432
 
   client_socket = socket(AF_INET, SOCK_STREAM)
   client_socket.connect((proxy_ip, proxy_port))
 
-  msg = input('Input message: ')
+  # msg = input('Input message: ')
+  msg = 'GET http://zhiju.me/networks/valid.html HTTP/1.1'
+  # msg = 'GET http://example.com/index.html HTTP/1.1'
   client_socket.send(msg.encode())
 
   modified_msg = client_socket.recv(1024)
