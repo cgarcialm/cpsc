cd6ef6216454071399d8a80dc083c3e9db5efbe3
Cecilia Garcia Lopez de Munain <ceci.garcialm@gmail.com>
2023-10-09 23:02:00 -0700

    Refactor into functions and add comments

--- 5510/Project1/Proxy/proxy.py
+++ 5510/Project1/Proxy/proxy.py
@@ -72,7 +72,7 @@ class ProxyServer:
         
         return received_data
 
-    def is_msg_length_valid(self, msg):
+    def is_valid_http_message_length(self, msg):
         """
         Check if the length of the received message is valid (should be 3).
 
@@ -84,7 +84,7 @@ class ProxyServer:
         """
         return len(msg.split()) == 3
 
-    def parse_http_msg(self, msg):
+    def parse_http_request(self, msg):
         """
         Parse an HTTP message into its components.
 
@@ -100,7 +100,7 @@ class ProxyServer:
         version = msg_components[2]
         return method, url, version
 
-    def is_http_version_correct(self, version):
+    def is_valid_http_version(self, version):
         """
         Check if the HTTP version is correct (should be 'HTTP/1.1').
 
@@ -112,7 +112,7 @@ class ProxyServer:
         """
         return version == 'HTTP/1.1'
 
-    def is_method_get(self, method):
+    def is_http_get_method(self, method):
         """
         Check if the HTTP method is GET.
 
@@ -124,7 +124,7 @@ class ProxyServer:
         """
         return method == 'GET'
 
-    def get_cache_path(self, url):
+    def get_cache_file_path(self, url):
         """
         Get the cache file path for a given URL.
 
@@ -147,9 +147,9 @@ class ProxyServer:
         Returns:
             bool: True if a cache file exists, False otherwise.
         """
-        return self.get_cache_path(url).exists()
+        return self.get_cache_file_path(url).exists()
 
-    def read_cache(self, url):
+    def read_cache_file_contents(self, url):
         """
         Read the contents of a cache file for a given URL.
 
@@ -159,11 +159,11 @@ class ProxyServer:
         Returns:
             str: The contents of the cache file.
         """
-        path = self.get_cache_path(url)
+        path = self.get_cache_file_path(url)
         file = path.open()
         return file.read()
 
-    def write_cache(self, url, msg):
+    def write_to_cache_file(self, url, msg):
         """
         Write an HTTP response to a cache file for a given URL.
 
@@ -171,11 +171,11 @@ class ProxyServer:
             url (str): The URL.
             msg (str): The HTTP response message.
         """
-        path = self.get_cache_path(url)
+        path = self.get_cache_file_path(url)
         path.parent.mkdir(parents=True, exist_ok=True)
         path.write_text(msg)
 
-    def create_msg_to_server(self, url):
+    def create_http_request_to_server(self, url):
         """
         Create an HTTP request message to be sent to the origin server.
 
@@ -189,7 +189,7 @@ class ProxyServer:
         host = urlparse(url).hostname
         return "GET {} HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n".format(path, host)
 
-    def get_response_status(self, msg):
+    def get_http_response_status(self, msg):
         """
         Get the HTTP response status code.
 
@@ -205,7 +205,7 @@ class ProxyServer:
         except ValueError:
             return -1
 
-    def process_server_message(self, url, msg):
+    def process_http_response_from_server(self, url, msg):
         """
         Process the HTTP response message from the origin server.
 
@@ -216,18 +216,69 @@ class ProxyServer:
         Returns:
             str: The processed HTTP response message.
         """
-        version = self.get_response_status(msg)
+        version = self.get_http_response_status(msg)
+        
         if len(msg) > 0 and version in ["200", "404"]:
+            # Extract the response body after headers
             msg = msg[msg.find("\r\n\r\n"):] + "\r\n\r\n"
+            
             if version == "200":
+                # Cache the response and modify cache headers for a 200 response
                 print("Response received from the server, and status code is 200! Writing to cache for future use...")
-                self.write_cache(url, "HTTP/1.1 200 OK\r\nCache Hit:1\r\n" + msg)
-                msg = "HTTP/1.1 200 OK\r\nCache Hit:0\n" + msg
+                self.write_to_cache_file(url, "HTTP/1.1 200 OK\r\nCache Hit: 1\r\n" + msg)
+                msg = "HTTP/1.1 200 OK\r\nCache Hit: 0\n" + msg
             else:
-                msg = "HTTP/1.1 404 Not Found\r\nCache Hit:0\r\n" + msg
+                # Modify cache headers for a 404 response
+                msg = "HTTP/1.1 404 Not Found\r\nCache Hit: 0\r\n" + msg
+            
             return msg
         
-        return "HTTP/1.1 500 Internal Server Error\r\nCache Hit:0\r\n\r\n"
+        # Handle unexpected response
+        return "HTTP/1.1 500 Internal Server Error\r\nCache Hit: 0\r\n\r\n"
+
+    def get_and_process_server_msg(self, client_msg):
+        """
+        Get and process the HTTP response from the origin server based on the client request.
+
+        Args:
+            client_msg (str): The HTTP request message from the client.
+
+        Returns:
+            str: The HTTP response message to send back to the client.
+        """
+        if not self.is_valid_http_message_length(client_msg):
+            # Handle an invalid client message length
+            server_msg = "Message length incorrect. Should be 3."
+        else:
+            method, url, version = self.parse_http_request(client_msg)
+            if not self.is_http_get_method(method):
+                # Handle an invalid HTTP method
+                server_msg = "Method incorrect. Should be GET."
+            elif not self.is_valid_http_version(version):
+                # Handle an invalid HTTP version
+                server_msg = "HTTP version incorrect. Should be HTTP/1.1."
+            else:
+                if self.cache_exists(url):
+                    # Serve from cache if the requested file is present
+                    print("Yeah! The requested file is in the cache and is about to be sent to the client!")
+                    server_msg = self.read_cache_file_contents(url)
+                else:
+                    # Request the file from the origin server
+                    print("Oops! No cache hit! Requesting origin server for the file...")
+                    client_socket = socket(AF_INET, SOCK_STREAM)
+                    msg_to_server = self.create_http_request_to_server(url)
+                    client_socket.connect((urlparse(url).hostname, self.server_port))
+                    print("Sending the following message from proxy to server:\r\n", msg_to_server)
+                    client_socket.send(msg_to_server.encode())
+
+                    # Receive and process the response from the origin server
+                    server_msg = self.receive_http_response(client_socket).decode()
+                    server_msg = self.process_http_response_from_server(url, server_msg)
+                    
+                    client_socket.close()
+                    print("Now responding to the client...")
+
+        return server_msg
 
     def run(self):
         """
@@ -240,34 +291,11 @@ class ProxyServer:
             print("Received a client connection from: (\'{}\', {})".format(client_ip, client_port))
             client_msg = conn_socket.recv(self.buf_size).decode()        
             print("Received a message from this client: b{}".format(repr(client_msg)))
-
-            if not self.is_msg_length_valid(client_msg):
-                server_msg = "Message length incorrect. Should be 3."
-            else:
-                method, url, version = self.parse_http_msg(client_msg)
-                if not self.is_method_get(method):
-                    server_msg = "Method incorrect. Should be GET."
-                elif not self.is_http_version_correct(version):
-                    server_msg = "HTTP version incorrect. Should be HTTP/1.1."
-                else:
-                    if self.cache_exists(url):
-                        print("Yeah! The requested file is in the cache and is about to be sent to the client!")
-                        server_msg = self.read_cache(url)
-                    else:
-                        print("Oops! No cache hit! Requesting origin server for the file...")
-                        client_socket = socket(AF_INET, SOCK_STREAM)
-                        msg_to_server = self.create_msg_to_server(url)
-                        client_socket.connect((urlparse(url).hostname, self.server_port))
-                        print("Sending the following message from proxy to server:\r\n", msg_to_server)
-                        client_socket.send(msg_to_server.encode())
-
-                        # TODO: Add response validation for other cases
-                        server_msg = self.receive_http_response(client_socket).decode()
-                        server_msg = self.process_server_message(url, server_msg)
-                        
-                        client_socket.close()
-                        print("Now responding to the client...")
-
+            
+            # Get and process the server response based on the client request
+            server_msg = self.get_and_process_server_msg(client_msg)
+            
+            # Send the response to the client
             conn_socket.send(server_msg.encode())
             conn_socket.close()
             print("All done! Closing socket...")
